48b9ee4e6de33ff41b5795da2ddd23a1d224f4ff
qzhou19 <zhouqianrr@gmail.com>
2021-06-27 15:38:27 -0500

    adding locust related files

--- locustfile.py
+++ locustfile.py
@@ -1,19 +1,18 @@
-import time
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2.5)
 
     @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+    def task_1(self):
+        self.client.get("")
 
     @task(3)
-    def view_items(self):
-        for item_id in range(10):
-            self.client.get(f"/item?id={item_id}", name="/item")
-            time.sleep(1)
-
-    def on_start(self):
-        self.client.post("/login", json={"username":"foo", "password":"bar"})
+    def task_2(self):
+        self.client.post("/predict", json={
+            "CHAS":{"0":0},
+            "RM":{"0":6.575},
+            "TAX":{"0":296.0},
+            "PTRATIO":{"0":15.3},
+            "B":{"0":396.9},
+            "LSTAT":{"0":4.98}})
